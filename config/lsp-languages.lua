ac25b39d9e74e7227438d56374539ac2accd1ee0
Rohit <email@domain.com>
2022-09-08 00:10:31 +0900

    fix diagnostics toggle

--- config/lsp-languages.lua
+++ config/lsp-languages.lua
@@ -4,26 +4,17 @@ require'lspconfig'.bashls.setup{}
 
 vim.g.diagnostics_active = true
 function _G.toggle_diagnostics()
+  vim.g.diagnostics_active = not vim.g.diagnostics_active
   if vim.g.diagnostics_active then
-    vim.g.diagnostics_active = false
-    vim.lsp.diagnostic.clear(0)
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
+	vim.api.nvim_echo({ { "Diagnostics enabled" } }, false, {})
+	vim.diagnostic.enable()
   else
-    vim.g.diagnostics_active = true
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-      vim.lsp.diagnostic.on_publish_diagnostics, {
-        virtual_text = true,
-        signs = true,
-        underline = true,
-        update_in_insert = false,
-      }
-    )
+	vim.api.nvim_echo({ { "Diagnostics disabled" } }, false, {})
+	vim.diagnostic.disable()
   end
 end
 
--- vim.api.nvim_set_keymap('n', '<leader>dd', ':call v:lua.toggle_diagnostics()<CR>',  {noremap = true, silent = true})
-require'toggle_lsp_diagnostics'.init()
-vim.api.nvim_set_keymap('n', '<leader>dd', ':ToggleDiag<CR>',  {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>dd', ':call v:lua.toggle_diagnostics()<CR>',  {noremap = true, silent = true})
 
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   virtual_text = true,

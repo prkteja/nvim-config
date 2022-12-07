a33344bf6defaac32d8ab01bfba0c92152134b88
Rohit <email@domain.com>
2022-12-07 23:58:56 +0900

    declutter nvim tree gutter

--- config/nvim-tree.lua
+++ config/nvim-tree.lua
@@ -104,10 +104,10 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
     enable = true,
     show_on_dirs = true,
     icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
+      hint = "•",
+      info = "•",
+      warning = "•",
+      error = "•",
     },
   },
   filters = {
